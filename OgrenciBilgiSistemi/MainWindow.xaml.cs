753307e85faedb259974e80310fe02be3cef062a
Alexandr <aleksandrpustovit8@gmail.com>
2026-05-11 18:24:23 +0300

    Add project files.

---
+++ OgrenciBilgiSistemi/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using OgrenciBilgiSistemi.Models;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OgrenciBilgiSistemi.Models;
+using System.IO;            //
+using System.Text.Json; // оба нужны что бы сохранить все на наш сд 
+
+namespace OgrenciBilgiSistemi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Ogrenci activeOgrenci = new Ogrenci();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                activeOgrenci.Ad = TxtAd.Text;
+                activeOgrenci.Soyad = TxtSoyad.Text;
+                activeOgrenci.OgrenciFakulte = TxtBolum.Text;
+                if(int.TryParse(TxtNumara.Text , out int numara))
+                {
+                    activeOgrenci.OgrenciNumara = numara; 
+                }else
+                {
+                    MessageBox.Show("Numarada Sadece Sayilar olmalu");
+                }
+
+
+            }
+            catch (Exception ex)
+            { 
+                MessageBox.Show(ex.Message);
+            } 
+            
+
+
+
+        }
+        //привязка нашей кнопки с текстом ввода 
+        private void BtnAddDers_Click(object sender, RoutedEventArgs e)
+        {
+            string newDers = TxtNewDers.Text;
+            if (! string.IsNullOrWhiteSpace(newDers))
+            {
+        
+                activeOgrenci.DersEkle(newDers);
+
+                
+                DersListi.ItemsSource = null; 
+                DersListi.ItemsSource = activeOgrenci.AlanDersler; 
+
+             
+                ToplamDers.Text = activeOgrenci.AlanDersler.Count.ToString();
+
+                TxtNewDers.Clear();
+            }
+        }
+
+
+
+
+
+
+        private void DersListi_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.Key == Key.Delete)
+                {
+                    if (DersListi.SelectedItem is string SecenDers)
+                    {
+                        activeOgrenci.DersiSil(SecenDers);
+
+                        DersListi.ItemsSource = null;
+                        DersListi.ItemsSource = activeOgrenci.AlanDersler;
+
+
+                        ToplamDers.Text = activeOgrenci.AlanDersler.Count.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // ошибкa вместо того, чтобы закрыть прогу
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+        }
+
+   
+    }
+}
---
+++ OgrenciBilgiSistemi/Models/Ogrenci.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Packaging;
+using System.Net.WebSockets;
+using System.Text;
+using System.Windows;
+
+namespace OgrenciBilgiSistemi.Models
+{
+    internal class Ogrenci
+    {
+        private string _ogr_ad;
+        private string _ogr_soyad;
+        private int _ogr_numara;
+        private string _ogr_fakulte;
+        private List<string> _alan_dersler = new List<string>();
+
+        public string Ad
+        {
+            get { return _ogr_ad; }
+            set
+            {
+                if (value == _ogr_ad) return;
+
+                _ogr_ad = value;
+            }
+        }
+        public string Soyad
+        {
+            get { return _ogr_soyad; }
+            set
+            {
+                if (value == _ogr_soyad) return;
+
+                _ogr_soyad = value;
+            }
+        }
+        public List<string> AlanDersler
+        {
+            get
+            {
+                return _alan_dersler;
+            }
+        }
+        public void AlanDersEkle(string DersName)
+        {
+            if (!_alan_dersler.Contains(DersName))
+            {
+                _alan_dersler.Add(DersName);
+
+            }
+            else
+            {
+                Console.WriteLine($"Error: This {DersName} have already contained");
+            }
+
+        }
+        public int OgrenciNumara
+        {
+            get { return _ogr_numara; }
+            set
+            {
+                if (value == _ogr_numara) return;
+
+                _ogr_numara = value;
+            }
+        }
+
+        public string OgrenciFakulte
+        {
+            get { return _ogr_fakulte; }
+            set
+            {
+                if (value == _ogr_fakulte) return;
+
+                _ogr_fakulte = value; 
+            }
+        }
+
+        public List<string> AlanDers
+        {
+            get { return _alan_dersler; }
+        }
+        public void DersEkle(string newDers)
+        {
+            if (!_alan_dersler.Contains(newDers))
+            {
+                _alan_dersler.Add(newDers);
+
+            }
+            else
+            {
+                MessageBox.Show("Böyle bir konu zaten var!");
+            }
+            
+
+        }
+        public void DersiSil(string Ders)
+        {
+            if (_alan_dersler.Contains(Ders))
+            {
+                _alan_dersler.Remove(Ders);
+            }
+            else return; 
+        }
+       
+    }
+
+}
